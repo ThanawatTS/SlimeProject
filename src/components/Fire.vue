10f05ae2ffd0d52517a2580bb8ad0299a5256381
Supawit Supparat <Supawit.sup@ku.th>
2019-02-02 20:17:03 +0700

    Nearby search v.1

--- src/components/Fire.vue
+++ src/components/Fire.vue
@@ -1,20 +1,53 @@
 <template>
-<div>
-  <b-card title="Card Title"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
-          class="mb-2">
-    <p class="card-text">
-      Some quick example text to build on the card title and make up the bulk of the card's content.
-    </p>
-    <b-button href="#" variant="primary">Go somewhere</b-button>
-  </b-card>
-</div>
+  <div id="app" class = "app">
+   
+   <div id= "List"  class="grid__text">
+    <List />
+   </div>
+   <div id = "Maps" class="grid__media"> 
+    <Maps />
+   </div>
+
+  </div>
 </template>
 
 <script>
-//For Test
+import List from "./List.vue";
+import Maps from "./Maps.vue";
+export default {
+  name: "App",
+  components: {
+    List,
+    Maps
+  }
+};
 </script>
+<style>
+#List{
+  display: block;
+  background: greenyellow;
+
+}
+#Maps{
+  
+}
+
+.app{
+  display: grid;
+  grid-template-columns: 
+  minmax(20px, 1fr) repeat(12, minmax(0, 100px)) minmax(20px, 1fr);
+  grid-column-gap: 20px;
+}
+.grid__media {
+  grid-column: 2 / 10; /* Start on Column 2 and end at the start of Column 9 */
+}
+
+.grid__text {
+  grid-column: 10 / 14; /* Start on Column 10 and end at the start of Column 14 */
+}
+
+.grid__media,
+.grid__text {
+  grid-row: 1; /* We want to keep our items on the same row */
+}
+</style>
--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -1,24 +1,42 @@
 <template>
-  <div> 
-     <button  @click="addMapMarkers">
-        Find Restuarant
-      </button>
+  <div class = "app"> 
+    <div id= "List"  class="grid__text">
+    <ul>
+     <li v-for="Restname in RestuarantLocation">{{ Restname.Name }}
+       <button @click="FindLocation(Restname.location.latitude,Restname.location.longitude)">
+          Find Location
+       </button>
+
+
+     </li>
+   
+  </ul>
+  </div>
+   <div id = "Maps" class="grid__media"> 
     <br>
     <gmap-map
       :center="mapCenter"
       :zoom="12"
       style="width:100%;  height: 400px;"
     >
-
       <gmap-marker
-         :key="index"
+        :key="index"
         v-for="(m, index) in markers"
         :position="m.position"
-        @click="center=m.position"
-        
+      ></gmap-marker>
 
-              ></gmap-marker>
+
+      <gmap-marker
+        :key="index"
+        v-for="(x, index) in Currentlocation"
+        :position="x.position"
+        @click="center=m.position"
+        ></gmap-marker>
+
+
+
     </gmap-map>
+     </div>
   </div>
 </template>
 
@@ -26,12 +44,13 @@
 import firebaseApp from './firebaseInit';
 import firebase from 'firebase/app'
 export default {
-  name: "GoogleMap",
+  name: "Maps",
   data() {
     return {
-      mapCenter: { lat: 50.508, lng: -73.587 },
+      mapCenter: { lat: 0, lng: 0 },
       markers: [],
       RestuarantLocation: [],
+      Currentlocation: [],
     };
   },
   firestore() {
@@ -45,19 +64,8 @@ export default {
   },
   
   methods: {
-    //add marker to the map
-     addMapMarkers: function()  { 
-      for (var i = 0; i < this.RestuarantLocation.length; i++){
-        // console.log(typeof(this.RestuarantLocation[i].location.latitude))
-      const marker = {
-          lat: this.RestuarantLocation[i].location.latitude,
-          lng: this.RestuarantLocation[i].location.longitude
-        };
-        this.markers.push({ position: marker });
-        // console.log(markers)
-        this.center = marker;
-      }
-    },
+   
+  
     //Find current location
     geolocate: function() {
       navigator.geolocation.getCurrentPosition(position => {
@@ -65,9 +73,51 @@ export default {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
+        console.log(position.coords.latitude)
+        this.Currentlocation.push({position : this.mapCenter})
       });
+      
     },
+    FindLocation : function (Latitude,Longtitude) {
+        this.markers.pop();
+        const marker = {
+          lat: Latitude,
+          lng: Longtitude
+        };
+        this.markers.push({ position: marker });
+        // console.log(markers)
+        this.center = marker;
 
+       }
   }
 };
 </script>
+<style>
+#List{
+  display: block;
+  background: greenyellow;
+
+}
+#Maps{
+  
+}
+
+.app{
+  display: grid;
+  grid-template-columns: 
+  minmax(20px, 1fr) repeat(12, minmax(0, 100px)) minmax(20px, 1fr);
+  grid-column-gap: 20px;
+}
+.grid__media {
+  grid-column: 2 / 10; /* Start on Column 2 and end at the start of Column 9 */
+}
+
+.grid__text {
+  grid-column: 10 / 14; /* Start on Column 10 and end at the start of Column 14 */
+}
+
+.grid__media,
+.grid__text {
+  grid-row: 1; /* We want to keep our items on the same row */
+}
+</style>
