0b00ac3c600f47a91cac8231f4fa4f73ca244109
ThanawatTS <Thanawatgam@hotmail.com>
2019-02-06 11:17:35 +0700

    clear dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,12 +1,6 @@
 <template>
     <div id="dashboard">
         <h1>Dashboard</h1>
-        <router-link to="/signup">
-            <h1>Sign Up</h1>
-        </router-link>
-        <router-link to="/signin">
-            <h1>Sign In</h1>
-        </router-link>
     </div>
 </template>
 
