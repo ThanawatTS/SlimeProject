aa81d9819dccf40582d460c514dbd67cdecb6869
ThanawatTS <Thanawatgam@hotmail.com>
2019-03-12 01:17:05 +0700

    check duplicate input name && message pop-up when user input the name that already in list

--- src/components/Suggestion.vue
+++ src/components/Suggestion.vue
@@ -9,7 +9,8 @@
                         <form>
                             <label> Add Restaurant List </label><br/>
                             <input v-model="restaurant_Name" id="restaurantName" placeholder="Restaurant Name"><br/>
-                            <v-btn small color="primary" v-on:click.prevent="addedRestaurantList" :disabled="restaurant_Name == emptyName">Restaurant Name</v-btn>
+                            <span v-if="seen"> {{ message_duplicatename }} <br/></span>
+                            <v-btn small color="primary" v-on:click.prevent="addedRestaurantList(restaurant_Name)" :disabled="restaurant_Name == emptyName">Restaurant Name</v-btn>
                         </form>
                         
                         <ul>
@@ -36,7 +37,7 @@
                 <v-layout align-center justify-center column fill-height>
                 <h1> Random Restaurant</h1>
 
-                <v-btn small color="orange" v-on:click="randomRestaurant(restaurant_List.length)" :disabled="suggestion_restaurant != emptyName">Random</v-btn>
+                <v-btn small color="orange" v-on:click="randomRestaurant(restaurant_List.length)" :disabled="suggestion_restaurant != emptyName || restaurant_List.length == 0">Random</v-btn>
                 <span> Random : {{ suggestion_restaurant }}</span>
                 </v-layout>
 
@@ -50,6 +51,8 @@
 
 
 <script>
+import firebaseApp from './firebaseInit';
+var UserTest = firebaseApp.collection("users").doc("2");
 export default {
     name: 'suggestion',
     data () {
@@ -58,27 +61,63 @@ export default {
             restaurant_List: [],
             restaurant_ID: 1,
             emptyName: '',
-            suggestion_restaurant: ''
+            suggestion_restaurant: '',
+            message_duplicatename: 'Already have one',
+            seen: false
+            
         }
     },
     methods: {
-        addedRestaurantList(){
-            this.restaurant_List.push({
+        addedRestaurantList(name){
+            var checkduplicateName = false;
+            if(this.restaurant_List.length == 0){
+                this.restaurant_List.push({
                 id: this.restaurant_ID++,
                 Name: this.restaurant_Name
+                })
+                this.restaurant_Name = ''
+            } else {
+                for(var x in this.restaurant_List){
+                    if(name == this.$data.restaurant_List[x].Name){
+                        this.restaurant_Name = ''
+                        checkduplicateName = true;
+                        this.$data.seen = true;
+                        setTimeout(() => {
+                            this.$data.seen = false;
+                            }, 2000);
+                    }
+                }
+
+                if(checkduplicateName == false){
+                this.restaurant_List.push({
+                    id: this.restaurant_ID++,
+                    Name: this.restaurant_Name
+                    })
+                this.restaurant_Name = ''
+                
+                }
+            }
+
+            
+
+            UserTest.set({
+                name: "One",
+                menu: this.$data.restaurant_List
             })
-            this.restaurant_Name = ''
         },
         removeRestaurantName(index){
             this.restaurant_List.splice(index,1)
+            UserTest.update({
+                menu: this.$data.restaurant_List
+            })
         },
         randomRestaurant(num){
             this.$data.suggestion_restaurant = this.$data.restaurant_List[Math.floor(Math.random()*num)].Name
-            console.log(Math.floor(Math.random()*num))
             setTimeout(() => {
                 this.$data.suggestion_restaurant = ''
             }, 2000);
         }
+        
     }
 }
 </script>
