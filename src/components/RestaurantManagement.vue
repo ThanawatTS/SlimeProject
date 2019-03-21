08d77606ff1fc3ca687ef2e2f493e98079c30bc3
ThanawatTS <Thanawatgam@hotmail.com>
2019-03-22 00:42:52 +0700

    Refresh page will not overwrite the last one

--- src/components/RestaurantManagement.vue
+++ src/components/RestaurantManagement.vue
@@ -52,7 +52,7 @@ export default {
             var restaurantExist = restaurantCreate;
             var checkName = this.$data.restuarantName
             var existName = false;
-
+            
             restaurantExist.get().then(function(doc) {
                 if (doc.exists){
                     for(var checkExistName in doc.data().Restaurant.RestaurantName){
@@ -79,21 +79,19 @@ export default {
 
                     this.$data.restaurantEach.name = this.$data.restuarantName
                     var tar = this.iterationCopy(this.$data.restaurantEach)
-                    
 
-                    
-                        this.$data.restuarantInfo.push(tar)
-                        console.log(Object.values(tar))
-                        //console.log(object.values(this.$data.restuarantInfo))
-                        var restaurantData = {
-                            Restaurant: {
-                                RestaurantName: this.$data.restuarantInfo,
-                            }
+                    this.$data.restuarantInfo.push(tar)
+                    console.log(Object.values(tar))
+                    //console.log(object.values(this.$data.restuarantInfo))
+                    var restaurantData = {
+                        Restaurant: {
+                            RestaurantName: this.$data.restuarantInfo,
                         }
-                        restaurantCreate.set(restaurantData).then( function() {
-                            console.log("DOC Created!")
-                            this.$data.restaurantEach.name = ""
-                        })
+                    }
+                    restaurantCreate.set(restaurantData).then( function() {
+                        console.log("DOC Created!")
+                        this.$data.restaurantEach.name = ""
+                    })
                     
                     
                     
@@ -128,10 +126,48 @@ export default {
                 }
             }
             return target;
+        },
+        loadData(){
+            var user = firebase.auth().currentUser;
+                if(user){
+                    this.$data.userCur = user.email;
+                    console.log(this.$data.userCur)
+                } else {
+                    console.log("Didn't login yet")
+                }
+
+            
+            var restaurantData = firebaseApp.collection("RestaurantByUser").doc(this.$data.userCur)
+            var temparr
+            restaurantData.get().then(function(doc) {
+                console.log("1")
+                if(doc.exists){
+                    console.log("2")
+                    // for(var x in doc.data().Restaurant.RestaurantName){
+                    //     restuarantInfo = doc.data().Restaurant.RestaurantName
+                    // }
+                    console.log("2"+ doc.data().Restaurant.RestaurantName)
+                    temparr = doc.data().Restaurant.RestaurantName
+                    console.log("loadData" + temparr)
+                } else {
+                    console.log("NO Doc")
+                }
+            })
+            setTimeout(() => {
+                console.log("DATA" + temparr)  
+                this.$data.restuarantInfo = temparr
+                console.log(this.$data.restuarantInfo)
+            }, 1500);
+            
+        
         }
     },
     beforeMount(){
         this.checkStatus()
+        setTimeout(() => {
+            this.loadData()
+        }, 1500);
+       
     }
 }
 
