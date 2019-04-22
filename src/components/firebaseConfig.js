9676b81097b0b3b6ec9f9768fe99c7c03057016c
Supawitsup <supawit.sup@ku.th>
2019-04-22 11:48:22 +0700

    style v2

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCw-XfS4J2NNYEsedTbrKtrFX5Ymg8QBhc",
+    authDomain: "slimeslam-24d26.firebaseapp.com",
+    databaseURL: "https://slimeslam-24d26.firebaseio.com",
+    projectId: "slimeslam-24d26",
+    storageBucket: "slimeslam-24d26.appspot.com",
+    messagingSenderId: "1057978780971"
+}
